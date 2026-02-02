a0d9fca3cd8e57ec6049da99557998f74e9871f9
calebk5 <kastnercaleb@gmail.com>
2026-02-02 12:31:33 -0500

    feat: implement split section layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,33 @@
 import HeroHome from '@/components/HeroHome'
+import SplitSection from '@/components/SplitSection'
 
 export default function Home() {
     return (
         <main>
             <HeroHome />
+
+            <SplitSection
+                leftTitle="Lorem ipsum dolor sit"
+                leftParagraph="Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Accusamus at illum alias temporibus autem vel quos
+                        ducimus impedit nobis molestiae facere pariatur ex
+                        quaerat sed beatae laudantium necessitatibus, laboriosam
+                        in.Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Accusamus at illum alias temporibus autem vel quos
+                        ducimus impedit nobis molestiae facere pariatur ex
+                        quaerat sed beatae laudantium necessitatibus, laboriosam
+                        in. Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Accusamus at illum alias temporibus autem vel quos
+                        ducimus impedit nobis molestiae facere pariatur ex
+                        quaerat sed beatae laudantium necessitatibus, laboriosam
+                        in.Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Accusamus at illum alias temporibus autem vel quos
+                        ducimus impedit nobis molestiae facere pariatur ex
+                        quaerat sed beatae laudantium necessitatibus, laboriosam
+                        in."
+                imageSrc="/Triplex.png"
+                imageAlt="Lorem ipsum dolor sit"
+            />
         </main>
     )
 }
