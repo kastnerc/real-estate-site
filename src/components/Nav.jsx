d207fdac0d295d99274dc647b95c7d4ff78f49b4
calebk5 <kastnercaleb@gmail.com>
2026-02-02 16:07:30 -0500

    style: add navbar hover animation and button styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,25 +3,50 @@ import Link from 'next/link'
 export default function Nav() {
     return (
         <nav className="z-10 absolute w-full h-20 flex items-center justify-between bg-linear-to-b from-black/70 to-transparent text-white">
-            <Link href="/" className="mx-10 text-2xl no-underline font-light uppercase">
+            <Link
+                href="/"
+                className="mx-10 text-2xl no-underline font-light uppercase"
+            >
                 GK <br />
                 Immobilier
             </Link>
-            <ul className="list-none flex m-0 p-0">
+            <ul className="list-none flex items-center m-0 p-0">
                 <li className="mx-10 relative">
-                    <Link href="/" className="no-underline uppercase">
-                        Our Projects
+                    <Link
+                        href="/"
+                        className="relative group inline-block uppercase"
+                    >
+                        <span className="relative z-10">Contact</span>
+
+                        <span
+                            className="
+                            absolute left-0 -bottom-1 h-px w-full
+                            bg-current scale-x-0 origin-left
+                            transition-transform duration-300 ease-out group-hover:scale-x-100
+                        "
+                        />
                     </Link>
                 </li>
 
                 <li className="mx-10 relative">
-                    <Link href="/" className="no-underline uppercase">
-                        About Us
+                    <Link
+                        href="/"
+                        className="relative group inline-block uppercase"
+                    >
+                        <span className="relative z-10">About Us</span>
+
+                        <span
+                            className="
+                            absolute left-0 -bottom-1 h-px w-full
+                            bg-current scale-x-0 origin-left
+                            transition-transform duration-300 ease-out group-hover:scale-x-100
+                        "
+                        />
                     </Link>
                 </li>
 
-                <li className="mx-10 relative">
-                    <Link href="/" className="no-underline uppercase">
+                <li className="mx-10 backdrop-blur-sm relative border border-white p-2 hover:bg-white hover:text-black transition-colors duration-300 ease-out">
+                    <Link href="/" className="block w-full h-full text-center no-underline uppercase">
                         Contact Us
                     </Link>
                 </li>
