aee6a0970cd030daf7db7763629e155e201bf6f9
calebk5 <kastnercaleb@gmail.com>
2026-02-02 13:55:12 -0500

    feat: add parallax image background to CardSection

--- src/components/CardSection.jsx
+++ src/components/CardSection.jsx
@@ -1,11 +1,18 @@
 export default function CardSection() {
     return (
-        <section className="p-10 bg-black">
-            <div className="text-center">
+        <section className="relative overflow-hidden p-10">
+            <div
+                className="absolute inset-0 bg-center bg-cover bg-fixed"
+                style={{ backgroundImage: `url('/Gatineau Skyline.jpg')` }}
+            ></div>
+
+            <div className="absolute top-0 left-0 w-full h-full bg-black/20"></div>
+
+            <div className="relative text-center">
                 <h1 className="text-2xl uppercase text-white">Our Approach</h1>
             </div>
-            <div className="grid grid-cols-3 gap-10 p-10 ">
-                <div className="text-center border border-white px-5 py-3">
+            <div className="relative grid grid-cols-1 md:grid-cols-3 gap-10 p-10 ">
+                <div className="text-center backdrop-blur-md border border-white px-5 py-3">
                     <h2 className="text-xl uppercase text-white">
                         Lorem ipsum dolor
                     </h2>
@@ -18,7 +25,7 @@ export default function CardSection() {
                     </p>
                 </div>
 
-                <div className="text-center border border-white px-5 py-3">
+                <div className="text-center backdrop-blur-md border border-white px-5 py-3">
                     <h2 className="text-xl uppercase text-white">
                         Lorem ipsum dolor
                     </h2>
@@ -31,7 +38,7 @@ export default function CardSection() {
                     </p>
                 </div>
 
-                <div className="text-center border border-white px-5 py-3">
+                <div className="text-center backdrop-blur-md border border-white px-5 py-3">
                     <h2 className="text-xl uppercase text-white">
                         Lorem ipsum dolor
                     </h2>
