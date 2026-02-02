5545ffab97ce4637faab6e1f1e8fba5ca78bb5ef
calebk5 <kastnercaleb@gmail.com>
2026-02-02 12:59:19 -0500

    feat: add card section component

---
+++ src/components/CardSection.jsx
@@ -0,0 +1,49 @@
+export default function CardSection() {
+    return (
+        <section className="p-10 bg-black">
+            <div className="text-center">
+                <h1 className="text-2xl uppercase text-white">Our Approach</h1>
+            </div>
+            <div className="grid grid-cols-3 gap-10 p-10 ">
+                <div className="text-center border border-white px-5 py-3">
+                    <h2 className="text-xl uppercase text-white">
+                        Lorem ipsum dolor
+                    </h2>
+                    <p className="px-5 text-white">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Obcaecati corrupti eveniet facilis officiis expedita
+                        quidem iusto aliquid incidunt accusamus iure. Quos sequi
+                        hic debitis, quisquam laudantium cumque rerum
+                        consectetur sapiente!
+                    </p>
+                </div>
+
+                <div className="text-center border border-white px-5 py-3">
+                    <h2 className="text-xl uppercase text-white">
+                        Lorem ipsum dolor
+                    </h2>
+                    <p className="px-5 text-white">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Obcaecati corrupti eveniet facilis officiis expedita
+                        quidem iusto aliquid incidunt accusamus iure. Quos sequi
+                        hic debitis, quisquam laudantium cumque rerum
+                        consectetur sapiente!
+                    </p>
+                </div>
+
+                <div className="text-center border border-white px-5 py-3">
+                    <h2 className="text-xl uppercase text-white">
+                        Lorem ipsum dolor
+                    </h2>
+                    <p className="px-5 text-white">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Obcaecati corrupti eveniet facilis officiis expedita
+                        quidem iusto aliquid incidunt accusamus iure. Quos sequi
+                        hic debitis, quisquam laudantium cumque rerum
+                        consectetur sapiente!
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
