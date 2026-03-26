c8edcae838cfc257cf0a3cc2533b6788e6b6bcef
calebk5 <kastnercaleb@gmail.com>
2026-03-26 13:39:24 -0400

    feat: add team section component

---
+++ src/components/TeamSection.jsx
@@ -0,0 +1,51 @@
+import Image from 'next/image'
+
+export default function TeamSection() {
+    return (
+        <section className="min-h-screen mx-auto w-[60vw]">
+            <div className="grid grid-cols-2 gap-10 px-10 py-10">
+                <div className="text-4xl uppercase">
+                    <h1>
+                        Our team of investors <br /> & experts
+                    </h1>
+                </div>
+                <div className="text-xl">
+                    <h2>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Omnis aut architecto molestiae ratione nisi
+                    </h2>
+                </div>
+            </div>
+            <div className="grid grid-cols-2 gap-10 p-10">
+                <div className="">
+                    <Image
+                        src="/images/Man.jpg"
+                        width={1080}
+                        height={1080}
+                        quality={75}
+                        alt="man"
+                    />
+
+                    <div className="bg-gray-100 p-5">
+                        <h3 className="font-bold text-xl">Loic Gendron</h3>
+                        <h4 className="text-large">Coproprietaire</h4>
+                    </div>
+                </div>
+                <div className="">
+                    <Image
+                        src="/images/Man.jpg"
+                        width={1080}
+                        height={1080}
+                        quality={75}
+                        alt="man"
+                    />
+
+                    <div className="bg-gray-100 p-5">
+                        <h3 className="font-bold text-xl">Loic Gendron</h3>
+                        <h4 className="text-large">Coproprietaire</h4>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
