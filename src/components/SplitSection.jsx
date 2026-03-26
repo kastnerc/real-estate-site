97c161fbb4759980923a754ab97bf4263a5b5e45
calebk5 <kastnercaleb@gmail.com>
2026-03-26 14:13:06 -0400

    fix: resolve split section button issue

--- src/components/SplitSection.jsx
+++ src/components/SplitSection.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function SplitSection({
     leftTitle,
@@ -12,8 +13,13 @@ export default function SplitSection({
                 <div>
                     <h1 className="text-3xl uppercase pb-4">{leftTitle}</h1>
                     <p>{leftParagraph}</p>
-                    <button className="self-start bg-transparent text-lg py-3 px-7 my-4 border-2 border-black uppercase">
-                        Button 1
+                    <button className="self-start bg-transparent text-lg my-4 border-2 border-black hover:bg-black hover:text-white transition-colors duration-300 ease-out uppercase">
+                        <Link
+                            href="/Contact"
+                            className="block w-full h-full py-3 px-7 text-center no-underline uppercase"
+                        >
+                            Our other projects
+                        </Link>
                     </button>
                 </div>
 
