9abdb9f9747f46a7c47469c304938a14a06107d5
calebk5 <kastnercaleb@gmail.com>
2026-03-26 13:55:02 -0400

    fix: deleted Home component from Contact and changed to Hero component

--- src/app/Contact/page.jsx
+++ src/app/Contact/page.jsx
@@ -1,14 +1,13 @@
-import Home from '@/components/Home'
+import Hero from '@/components/Hero'
 import SplitContactSection from '@/components/SplitContactSection'
 
 export default function Contact() {
     return (
         <main>
-            <Home
+            <Hero
                 title="Contact Us"
                 subtitle="Whether you have questions about what we do, investment opportunities, or potential partnerships, GK Immobilier would be happy to speak with you."
-                imageSrc="/images/Office picture.jpg"
-                imageAlt="Office"
+                imageSrc="'/images/Office picture.jpg'"
             />
 
             <SplitContactSection 
